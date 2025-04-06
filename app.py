6cb2b5b11bec3e0f52647c9dd8ffa636bbde12ff
sakshishaw0805 <sakshishaw0805@gmail.com>
2025-04-07 05:03:46 +0530

    Simplify app.py

--- app.py
+++ app.py
@@ -58,6 +58,4 @@ def recommend():
     results = search_assessments(query, model, index, assessments)
     return jsonify(results)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+app = Flask(__name__)
