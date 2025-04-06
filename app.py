c5285b8376a6a5fb66950ee54612429c827a518a
sakshishaw0805 <sakshishaw0805@gmail.com>
2025-04-07 04:15:42 +0530

    Simplify app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 import faiss
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 assessments = []
@@ -58,4 +59,5 @@ def recommend():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
